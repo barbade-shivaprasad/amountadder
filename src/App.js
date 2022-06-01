8cc9cfd9d977f1d98d848f6b3b5c9c325d1fdf5a
barbade-shivaprasad <78468138+barbade-shivaprasad@users.noreply.github.com>
2022-06-01 11:54:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [amount, setamount] = useState(0);
+
+  const add = (num) => {
+    setamount(amount + num);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h4>Amount adder</h4>
+      <div class="input-group m-auto w-25 mt-3">
+        <div class="input-group-prepend">
+          <span class="input-group-text">Rs</span>
+        </div>
+        <input
+          value={amount}
+          type="number"
+          class="form-control"
+          aria-label="Amount (to the nearest rupee)"
+          onChange={e=>setamount(parseInt(e.target.value))}
+        ></input>
+      </div>
+      <button className="btn btn-success m-2" onClick={(e) => add(500)}>
+        + 500
+      </button>
+      <button className="btn btn-success m-2" onClick={(e) => add(1000)}>
+        + 1000
+      </button>
+      <button className="btn btn-success m-2" onClick={(e) => add(5000)}>
+        + 5000
+      </button>
     </div>
   );
 }
